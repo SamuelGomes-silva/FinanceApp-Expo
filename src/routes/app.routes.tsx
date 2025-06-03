3a4d4961f466cd51a88e0f7d2139c0d59775af26
SamuelGomes-silva <samuelgs1106@gmail.com>
2025-06-02 21:18:25 -0300

    Criando drawer personalizado

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -6,6 +6,9 @@ import {
 	createDrawerNavigator,
 	DrawerNavigationProp,
 } from "@react-navigation/drawer";
+import CustomDrawerContent from "../components/customDrawer";
+import { colors } from "../constants/colors";
+import { Ionicons } from "@expo/vector-icons";
 
 const AppDrawer = createDrawerNavigator();
 
@@ -19,10 +22,52 @@ export type DrawerNav = DrawerNavigationProp<AppDrawerParamList>;
 
 export function AppRouteStack(): JSX.Element {
 	return (
-		<AppDrawer.Navigator screenOptions={{ headerShown: false }}>
-			<AppDrawer.Screen name="Home" component={Dashboard} />
-			<AppDrawer.Screen name="Register" component={Register} />
-			<AppDrawer.Screen name="Profile" component={Profile} />
+		<AppDrawer.Navigator
+			screenOptions={{
+				headerShown: false,
+				drawerActiveBackgroundColor: colors.indigoBlue,
+				drawerActiveTintColor: colors.white,
+				drawerInactiveBackgroundColor: colors.white,
+				drawerInactiveTintColor: colors.gray,
+				drawerItemStyle: {
+					marginBottom: 15,
+					borderRadius: 8,
+				},
+				drawerLabelStyle: {
+					fontSize: 18,
+					fontWeight: "bold",
+					marginLeft: 10,
+				},
+			}}
+			drawerContent={(props) => <CustomDrawerContent {...props} />}
+		>
+			<AppDrawer.Screen
+				name="Home"
+				component={Dashboard}
+				options={{
+					drawerIcon: ({ color, size }) => (
+						<Ionicons name="home-outline" color={color} size={size} />
+					),
+				}}
+			/>
+			<AppDrawer.Screen
+				name="Register"
+				component={Register}
+				options={{
+					drawerIcon: ({ color, size }) => (
+						<Ionicons name="add-circle" color={color} size={size} />
+					),
+				}}
+			/>
+			<AppDrawer.Screen
+				name="Profile"
+				component={Profile}
+				options={{
+					drawerIcon: ({ color, size }) => (
+						<Ionicons name="person" color={color} size={size} />
+					),
+				}}
+			/>
 		</AppDrawer.Navigator>
 	);
 }
